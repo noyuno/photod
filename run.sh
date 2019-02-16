91c6f0e210c91199b5e4dbfad6f9c16fab4dc2f4
noyuno <noyuno@users.noreply.github.com>
2019-02-16 22:44:30 +0900

    Update

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
 #!/bin/sh -e
 mkdir -p ~/.aws
+
+if [ ! "$AWS_ACCESS_KEY" -o ! "$AWS_SECRET_KEY" -o ! "$AWS_REGION" ]; then
+    echo "Required environment variable not set. It requires AWS_ACCESS_KEY, AWS_SECRET_KEY, AWS_REGION" >&2
+    exit 1
+fi
+
 cat << EOF >~/.aws/credentials
 [default]
 aws_access_key_id = $AWS_ACCESS_KEY
